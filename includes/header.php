079a0129a2f28a86ea303669108c072b758b15fb
MwakaiJoseph <josephmwakaimalenge@gmail.com>
2026-02-20 23:38:50 +0300

    Bump CSS version string to fix mobile cache for login screen toggle overlap

--- includes/header.php
+++ includes/header.php
@@ -24,8 +24,8 @@ $base = $in_admin ? '..' : '.';
     <link rel="stylesheet" href="<?php echo $base; ?>/assets/css/vendor.css">
     <!-- link rel="stylesheet" href="<?php echo $base; ?>/assets/css/style.css" -->
     <!-- Light theme to align all pages with homepage look -->
-    <link rel="stylesheet" href="<?php echo $base; ?>/assets/css/theme-light.css?v=2">
-    <link rel="stylesheet" href="<?php echo $base; ?>/assets/css/header-custom.css?v=2">
+    <link rel="stylesheet" href="<?php echo $base; ?>/assets/css/theme-light.css?v=3">
+    <link rel="stylesheet" href="<?php echo $base; ?>/assets/css/header-custom.css?v=3">
     <?php if ($in_admin): ?>
       <link rel="stylesheet" href="<?php echo $base; ?>/assets/css/admin.css">
     <?php
